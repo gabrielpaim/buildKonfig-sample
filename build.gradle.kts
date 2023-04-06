50660cd19812f3cd7da85a9ba1011065684a13d6
gabrielpaim <gpaim@inpulse.med.br>
2023-04-06 18:35:15 -0300

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    kotlin("multiplatform") version "1.8.10"
+}
+
+group = "vet.inpulse"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvm {
+        jvmToolchain(11)
+        withJava()
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val jvmMain by getting
+        val jvmTest by getting
+    }
+}
